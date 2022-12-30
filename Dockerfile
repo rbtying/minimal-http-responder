9e6b90c1252424eebaddd53208d28f96e13ae756
Robert Ying <rbtying@aeturnalus.com>
2022-12-30 09:31:13 -0800

    fix sigterm handling

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ RUN cargo install --path .
 
 FROM scratch
 COPY --from=builder /usr/local/cargo/bin/minimal-http-responder /usr/local/bin/minimal-http-responder
-CMD ["minimal-http-responder"]
+ENTRYPOINT ["minimal-http-responder"]
--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,12 @@ struct Configuration {
 }
 
 fn main() {
+    ctrlc::set_handler(|| {
+        println!("Received SIGTERM");
+        std::process::exit(0);
+    })
+    .unwrap();
+
     let status_code = env::var("STATUS_CODE")
         .map_err(|_| ())
         .and_then(|s| StatusCode::from_bytes(s.as_bytes()).map_err(|_| ()))
@@ -22,13 +28,11 @@ fn main() {
 
     let listener = TcpListener::bind("0.0.0.0:2020").unwrap();
 
-    for stream in listener.incoming() {
-        if let Ok(s) = stream {
-            let c = Arc::clone(&configuration);
-            thread::spawn(move || {
-                let _ = handle_request(&c, s);
-            });
-        }
+    for stream in listener.incoming().flatten() {
+        let c = Arc::clone(&configuration);
+        thread::spawn(move || {
+            let _ = handle_request(&c, stream);
+        });
     }
 }
 
@@ -36,11 +40,11 @@ fn handle_request(configuration: &Configuration, mut stream: TcpStream) -> Resul
     let mut buf = [0u8; 1024];
     loop {
         let n = stream.read(&mut buf)?;
-        if n == 0 || buf.windows(4).position(|w| w == b"\r\n\r\n").is_some() {
+        if n == 0 || buf.windows(4).any(|w| w == b"\r\n\r\n") {
             break;
         }
     }
-    stream.write(
+    stream.write_all(
         format!(
             "HTTP/1.1 {} {}\r\n",
             configuration.status_code.as_u16(),
@@ -60,8 +64,9 @@ fn handle_request(configuration: &Configuration, mut stream: TcpStream) -> Resul
 
         (None, None) => format!("{}", configuration.status_code.as_u16()),
     };
-    stream.write(format!("Content-Length: {}\r\n\r\n", contents.as_bytes().len()).as_bytes())?;
-    stream.write(contents.as_bytes())?;
+    stream
+        .write_all(format!("Content-Length: {}\r\n\r\n", contents.as_bytes().len()).as_bytes())?;
+    stream.write_all(contents.as_bytes())?;
     stream.flush()?;
     Ok(())
 }
